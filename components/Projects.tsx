b2d75402682f1ffa648df505cb251e605ad32c27
Tran Phat <phat.trantan@h2-inc.com>
2023-02-14 09:01:45 +0700

    fix project components

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,191 +6,155 @@ import { AiOutlineGithub } from "react-icons/ai";
 type Props = {};
 
 const Projects = (props: Props) => {
-  gsap.registerPlugin(ScrollTrigger);
-
-  useEffect(() => {
-    const components = document.querySelectorAll("#component");
-    const container: HTMLElement = document.querySelector("#container")!;
-
-    gsap.to(components, {
-      xPercent: -100 * (components.length - 1),
-      ease: "none",
-      scrollTrigger: {
-        trigger: container,
-        pin: true,
-        scrub: 1,
-        snap: 1 / (components.length - 1),
-        end: () => "+=" + container.offsetWidth,
-      },
-    });
-  }, []);
-
   return (
-    // <motion.div className="flex relative flex-col text-center md:text-left px-7 md:px-10 justify-evenly mx-auto mt-24">
-    //   <div className="h-[200px]"></div>
-    //   <h3 className="uppercase tracking-[15px] text-gray-500 text-3xl md:text-6xl text-center mb-24">
-    //     Projects
-    //   </h3>
+    <motion.div className="flex relative flex-col text-center md:text-left px-7 md:px-10 justify-evenly mx-auto mt-24">
+      <div className="h-[200px]"></div>
+      <h3 className="uppercase tracking-[15px] text-gray-500 text-3xl md:text-6xl text-center mb-24">
+        Projects
+      </h3>
 
-    //   <div className="w-full absolute top-[30%] bg-primary/10 left-0 h-[500px] -skew-y-12 z-0"></div>
+      <div className="w-full absolute top-[30%] bg-primary/10 left-0 h-[500px] -skew-y-12 z-0"></div>
 
-    <div id="container" className="flex w-[500vw]">
-      <div id="component" className="one w-[100vw] h-[100vh]">
-        Primeiro
-      </div>
-      <div id="component" className="two w-[100vw] h-[100vh]">
-        Segundo
-      </div>
-      <div id="component" className="three w-[100vw] h-[100vh]">
-        Terceiro
-      </div>
-      <div id="component" className="four w-[100vw] h-[100vh]">
-        Quarto
-      </div>
-      <div id="component" className="five w-[100vw] h-[100vh]">
-        Quinto
-      </div>
-    </div>
-    // <div
-    //   id="container"
-    //   className="projects flex w-[200vw] mx-auto z-1 overflow-x-scroll"
-    // >
-    //   <div id="project" className="project-item !w-[100vw]">
-    //     <div className="grid grid-cols-1 md:grid-cols-2 md:p-10 bg-slate-500 rounded-lg shadow-md">
-    //       <div className="col-span-1 p-5 rounded-md">
-    //         <img
-    //           src="https://img.freepik.com/free-photo/vivid-blurred-colorful-background_58702-2545.jpg?w=2000"
-    //           alt=""
-    //           className="w-full h-full object-cover rounded-lg"
-    //         />
-    //       </div>
+      <div
+        id="container"
+        className="projects flex w-[200vw] mx-auto z-1 overflow-x-scroll"
+      >
+        <div id="project" className="project-item !w-[100vw]">
+          <div className="grid grid-cols-1 md:grid-cols-2 md:p-10 bg-slate-500 rounded-lg shadow-md">
+            <div className="col-span-1 p-5 rounded-md">
+              <img
+                src="https://img.freepik.com/free-photo/vivid-blurred-colorful-background_58702-2545.jpg?w=2000"
+                alt=""
+                className="w-full h-full object-cover rounded-lg"
+              />
+            </div>
 
-    //       <div className="col-span-1 p-3 md:p-5 my-auto">
-    //         <div className="title">
-    //           <h4 className="text-2xl md:text-4xl text-[#2c3e50] font-bold font-Pacifico">
-    //             Booking Webiste
-    //           </h4>
-    //         </div>
-    //         <div className="desc mt-5 text-white font-medium text-sm md:text-base">
-    //           Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-    //           Quidempisicing elit. Quidem cing elit. Quidem
-    //         </div>
-    //         <div className="date my-4">2022 - Dev</div>
-    //         <div className="tech flex gap-3 justify-center md:justify-start">
-    //           <motion.img
-    //             whileTap={{
-    //               scale: 1.3,
-    //             }}
-    //             src="/images/mongodb.png"
-    //             alt=""
-    //             className="w-[50px] h-[50px] border border-white/20 rounded-full p-2 shadow-md hover:border-white hover:bg-white transition duration-300"
-    //           />
-    //           <motion.img
-    //             whileTap={{
-    //               scale: 1.3,
-    //             }}
-    //             src="/images/mongodb.png"
-    //             alt=""
-    //             className="w-[50px] h-[50px] border border-white/20 rounded-full p-2 shadow-md hover:border-white hover:bg-white transition duration-300"
-    //           />
-    //           <motion.img
-    //             whileTap={{
-    //               scale: 1.3,
-    //             }}
-    //             src="/images/mongodb.png"
-    //             alt=""
-    //             className="w-[50px] h-[50px] border border-white/20 rounded-full p-2 shadow-md hover:border-white hover:bg-white transition duration-300"
-    //           />
-    //         </div>
-    //         <div className="action my-5">
-    //           <motion.button
-    //             whileTap={{
-    //               scale: 1.2,
-    //             }}
-    //             whileHover={{
-    //               scale: 1.05,
-    //             }}
-    //             transition={{
-    //               duration: 0.2,
-    //             }}
-    //             className="border border-[#1b222b] bg-[#1b222b] py-3 px-4 rounded-lg shadow-sm flex items-center justify-center hover:opacity-90 transition-all duration-300"
-    //           >
-    //             <AiOutlineGithub className="inline-block text-2xl mr-2" />
-    //             View Github{" "}
-    //           </motion.button>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <div id="project" className="project-item !w-[100vw]">
-    //     <div className="grid grid-cols-1 md:grid-cols-2 md:p-10 bg-slate-500 rounded-lg shadow-md">
-    //       <div className="col-span-1 p-5 rounded-md">
-    //         <img
-    //           src="https://img.freepik.com/free-photo/vivid-blurred-colorful-background_58702-2545.jpg?w=2000"
-    //           alt=""
-    //           className="w-full h-full object-cover rounded-lg"
-    //         />
-    //       </div>
+            <div className="col-span-1 p-3 md:p-5 my-auto">
+              <div className="title">
+                <h4 className="text-2xl md:text-4xl text-[#2c3e50] font-bold font-Pacifico">
+                  Booking Webiste
+                </h4>
+              </div>
+              <div className="desc mt-5 text-white font-medium text-sm md:text-base">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quidempisicing elit. Quidem cing elit. Quidem
+              </div>
+              <div className="date my-4">2022 - Dev</div>
+              <div className="tech flex gap-3 justify-center md:justify-start">
+                <motion.img
+                  whileTap={{
+                    scale: 1.3,
+                  }}
+                  src="/images/mongodb.png"
+                  alt=""
+                  className="w-[50px] h-[50px] border border-white/20 rounded-full p-2 shadow-md hover:border-white hover:bg-white transition duration-300"
+                />
+                <motion.img
+                  whileTap={{
+                    scale: 1.3,
+                  }}
+                  src="/images/mongodb.png"
+                  alt=""
+                  className="w-[50px] h-[50px] border border-white/20 rounded-full p-2 shadow-md hover:border-white hover:bg-white transition duration-300"
+                />
+                <motion.img
+                  whileTap={{
+                    scale: 1.3,
+                  }}
+                  src="/images/mongodb.png"
+                  alt=""
+                  className="w-[50px] h-[50px] border border-white/20 rounded-full p-2 shadow-md hover:border-white hover:bg-white transition duration-300"
+                />
+              </div>
+              <div className="action my-5">
+                <motion.button
+                  whileTap={{
+                    scale: 1.2,
+                  }}
+                  whileHover={{
+                    scale: 1.05,
+                  }}
+                  transition={{
+                    duration: 0.2,
+                  }}
+                  className="border border-[#1b222b] bg-[#1b222b] py-3 px-4 rounded-lg shadow-sm flex items-center justify-center hover:opacity-90 transition-all duration-300"
+                >
+                  <AiOutlineGithub className="inline-block text-2xl mr-2" />
+                  View Github{" "}
+                </motion.button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="project" className="project-item !w-[100vw]">
+          <div className="grid grid-cols-1 md:grid-cols-2 md:p-10 bg-slate-500 rounded-lg shadow-md">
+            <div className="col-span-1 p-5 rounded-md">
+              <img
+                src="https://img.freepik.com/free-photo/vivid-blurred-colorful-background_58702-2545.jpg?w=2000"
+                alt=""
+                className="w-full h-full object-cover rounded-lg"
+              />
+            </div>
 
-    //       <div className="col-span-1 p-3 md:p-5 my-auto">
-    //         <div className="title">
-    //           <h4 className="text-2xl md:text-4xl text-[#2c3e50] font-bold font-Pacifico">
-    //             Booking Webiste
-    //           </h4>
-    //         </div>
-    //         <div className="desc mt-5 text-white font-medium text-sm md:text-base">
-    //           Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-    //           Quidempisicing elit. Quidem cing elit. Quidem
-    //         </div>
-    //         <div className="date my-4">2022 - Dev</div>
-    //         <div className="tech flex gap-3 justify-center md:justify-start">
-    //           <motion.img
-    //             whileTap={{
-    //               scale: 1.3,
-    //             }}
-    //             src="/images/mongodb.png"
-    //             alt=""
-    //             className="w-[50px] h-[50px] border border-white/20 rounded-full p-2 shadow-md hover:border-white hover:bg-white transition duration-300"
-    //           />
-    //           <motion.img
-    //             whileTap={{
-    //               scale: 1.3,
-    //             }}
-    //             src="/images/mongodb.png"
-    //             alt=""
-    //             className="w-[50px] h-[50px] border border-white/20 rounded-full p-2 shadow-md hover:border-white hover:bg-white transition duration-300"
-    //           />
-    //           <motion.img
-    //             whileTap={{
-    //               scale: 1.3,
-    //             }}
-    //             src="/images/mongodb.png"
-    //             alt=""
-    //             className="w-[50px] h-[50px] border border-white/20 rounded-full p-2 shadow-md hover:border-white hover:bg-white transition duration-300"
-    //           />
-    //         </div>
-    //         <div className="action my-5">
-    //           <motion.button
-    //             whileTap={{
-    //               scale: 1.2,
-    //             }}
-    //             whileHover={{
-    //               scale: 1.05,
-    //             }}
-    //             transition={{
-    //               duration: 0.2,
-    //             }}
-    //             className="border border-[#1b222b] bg-[#1b222b] py-3 px-4 rounded-lg shadow-sm flex items-center justify-center hover:opacity-90 transition-all duration-300"
-    //           >
-    //             <AiOutlineGithub className="inline-block text-2xl mr-2" />
-    //             View Github{" "}
-    //           </motion.button>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-    // </motion.div>
+            <div className="col-span-1 p-3 md:p-5 my-auto">
+              <div className="title">
+                <h4 className="text-2xl md:text-4xl text-[#2c3e50] font-bold font-Pacifico">
+                  Booking Webiste
+                </h4>
+              </div>
+              <div className="desc mt-5 text-white font-medium text-sm md:text-base">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quidempisicing elit. Quidem cing elit. Quidem
+              </div>
+              <div className="date my-4">2022 - Dev</div>
+              <div className="tech flex gap-3 justify-center md:justify-start">
+                <motion.img
+                  whileTap={{
+                    scale: 1.3,
+                  }}
+                  src="/images/mongodb.png"
+                  alt=""
+                  className="w-[50px] h-[50px] border border-white/20 rounded-full p-2 shadow-md hover:border-white hover:bg-white transition duration-300"
+                />
+                <motion.img
+                  whileTap={{
+                    scale: 1.3,
+                  }}
+                  src="/images/mongodb.png"
+                  alt=""
+                  className="w-[50px] h-[50px] border border-white/20 rounded-full p-2 shadow-md hover:border-white hover:bg-white transition duration-300"
+                />
+                <motion.img
+                  whileTap={{
+                    scale: 1.3,
+                  }}
+                  src="/images/mongodb.png"
+                  alt=""
+                  className="w-[50px] h-[50px] border border-white/20 rounded-full p-2 shadow-md hover:border-white hover:bg-white transition duration-300"
+                />
+              </div>
+              <div className="action my-5">
+                <motion.button
+                  whileTap={{
+                    scale: 1.2,
+                  }}
+                  whileHover={{
+                    scale: 1.05,
+                  }}
+                  transition={{
+                    duration: 0.2,
+                  }}
+                  className="border border-[#1b222b] bg-[#1b222b] py-3 px-4 rounded-lg shadow-sm flex items-center justify-center hover:opacity-90 transition-all duration-300"
+                >
+                  <AiOutlineGithub className="inline-block text-2xl mr-2" />
+                  View Github{" "}
+                </motion.button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </motion.div>
   );
 };
 
