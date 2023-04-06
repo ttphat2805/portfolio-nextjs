532d639c3fa35399d2897b44955bf2a1565ce5d9
Phat <phattranits>
2023-04-06 17:07:42 +0700

    fix enviroment sanity api version

--- sanity.ts
+++ sanity.ts
@@ -4,7 +4,7 @@ import createImageUrlBuilder from "@sanity/image-url";
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2022-11-15",
   useCdn: process.env.NODE_ENV === "production",
 };
 
